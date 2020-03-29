a25867e670bef44c4e5f40435d3e6efb324b4598
Leo Chen <leo@saltedherring.com>
2020-03-29 19:21:09 +1300

    remove space

--- src/components/blocks/TransacItem.vue
+++ src/components/blocks/TransacItem.vue
@@ -1,12 +1,7 @@
 <template>
 <div v-if="source" class="columns sales__body__item" @dblclick.prevent="view_transaction">
     <div class="column col-when">{{source.datetime}}</div>
-    <div class="column col-amount is-2">
-        <i v-if="source.payment_method == 'Cash'" class="far fa-money-bill-alt"></i>
-        <i v-if="source.payment_method == 'EFTPOS'" class="far fa-credit-card"></i>
-        <i v-if="source.payment_method == 'Voucher'" class="fas fa-ticket-alt"></i>
-        <i v-if="source.payment_method == 'Web Order'" class="fas fa-globe"></i>
-        {{source.amount.toDollar()}}</div>
+    <div class="column col-amount is-2"><i v-if="source.payment_method == 'Cash'" class="far fa-money-bill-alt"></i><i v-if="source.payment_method == 'EFTPOS'" class="far fa-credit-card"></i><i v-if="source.payment_method == 'Voucher'" class="fas fa-ticket-alt"></i><i v-if="source.payment_method == 'Web Order'" class="fas fa-globe"></i>{{source.amount.toDollar()}}</div>
     <div class="column col-count is-2 has-text-centered">{{source.item_count}}</div>
     <div class="column col-discount is-2 has-text-centered">{{source.discount ? source.discount : '- No -'}}</div>
     <div class="column col-by is-2">{{source.by}}</div>
